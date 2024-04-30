50730b845ab6661cafe476975fbf40ac12de53e3
Jeffreylwang94 <jefflwang94@gmail.com>
2024-04-30 14:39:57 -0700

    Rough logic for sequential operations and equal sign completed - still has bugs with persistent adding behavior

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
-function add(num1,num2){return num1 + num2}
-function subtract(num1,num2){return num1 - num2}
-function multiply(num1,num2){return num1*num2}
-function divide(num1,num2){return num1/num2}
+function add(num1,num2){return Number(num1) + Number(num2)}
+function subtract(num1,num2){return Number(num1) - Number(num2)}
+function multiply(num1,num2){return Number(num1)*Number(num2)}
+function divide(num1,num2){return Number(num1)/Number(num2)}
 
 function operate(inputFunction,num1,num2){
     return inputFunction(num1,num2);
@@ -15,35 +15,62 @@ let currNum1;
 let currNum2;
 let displayValue = '';
 let currOperator;
+let clearState = false;
+
 
 buttonGrid.addEventListener("click",(e) => {
     const isButton = e.target.nodeName === "BUTTON";
-    let clearState = false;
 
-    if(isButton && clearState){
+    if(isButton && clearState && !isNaN(e.target.textContent)){
         displayValue = '';
         outputSquare.textContent = displayValue;
+        console.log(0);
+        clearState = false;
     }
 
-    if(isButton && e.target.textContent != "=" 
-                && !(operandDecoder(e.target.textContent))){
+    if (e.target.textContent === "clr" || !isButton){
+        return;
+    }
+    if(!isNaN(e.target.textContent)){
         displayValue = displayValue + e.target.textContent;
         outputSquare.textContent = displayValue;
+        console.log(1);
     } else if (operandDecoder(e.target.textContent) && currNum1 === undefined){
         currNum1 = displayValue;
         currOperator = operandDecoder(e.target.textContent);
         clearState = true;
-    } else if (operandDecoder(e.target.textContent) && currNum1 != undefined){
-        displayValue = operate(currOperator,num1,displayValue);
+        console.log(2);
+    } else {
+        console.log(3);
+    // } else if (operandDecoder(e.target.textContent){
+        currNum2 = displayValue;
+        displayValue = operate(currOperator,currNum1,currNum2);
         currNum1 = displayValue;
         outputSquare.textContent = displayValue;
+        clearState = true;
+        if (operandDecoder(e.target.textContent)){
+            currOperator = operandDecoder(e.target.textContent);
+        }
+        // currNum2 = undefined;
+    // } else (e.target.textContent === "="){
+    //     if(currNum1 != undefined){
+    //         currNum1 = displayValue;
+    //     } else if ()
     }
 })
 
 
-const equalButton = document.querySelector("equals");
-const clearButton = document.querySelector("clear");
+const equalButton = document.querySelector(".equals");
+const clearButton = document.querySelector(".clear");
 
+
+clearButton.addEventListener("click",(e) =>{
+    currNum1 = undefined;
+    currNum2 = undefined;
+    currOperator = undefined;
+    displayValue = "";
+    outputSquare.textContent = displayValue;
+})
 
     // equalButton.addEventListener("click",(e) =>{
 
